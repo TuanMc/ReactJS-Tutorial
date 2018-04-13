02dabf1aa82cc11e1aa84bcb55188857cd98cd6d
TuanMc <maichungtuan@gmail.com>
2018-04-13 16:47:00 +0700

    Add real template

---
+++ src/components/Login/Login.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import '../../assets/css/sb-admin.min.css';
+import '../../assets/vendor/bootstrap/css/bootstrap.min.css';
+import '../../assets/vendor/font-awesome/css/font-awesome.min.css';
+
+export default class Login extends Component {
+  render() {
+    document.body.style.backgroundColor = "#343a40";
+    return (
+      <div className="container">
+        <div className="card card-login mx-auto mt-5">
+          <div className="card-header">Login</div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label>Email address</label>
+                <input className="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email" />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input className="form-control" id="exampleInputPassword1" type="password" placeholder="Password" />
+              </div>
+              <div className="form-group">
+                <div className="form-check">
+                  <label className="form-check-label">
+                    <input className="form-check-input" type="checkbox" /> Remember Password</label>
+                </div>
+              </div>
+              <a className="btn btn-primary btn-block" href="/home">Login</a>
+            </form>
+            <div className="text-center">
+              <a className="d-block small mt-3" href="">Register an Account</a>
+              <a className="d-block small" href="">Forgot Password?</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/body-component/body-component.js
+++ src/components/body-component/body-component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+// import { PlayerAPI } from '../app-api';
 import '../../assets/App.css';
 import 'font-awesome/css/font-awesome.min.css';
 
@@ -11,55 +12,54 @@ export default class Body extends React.Component {
 
     render() {
         return (
-            <table className="table table-dark table-hover" >
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Notes</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.props.item.map(value => {
-                            console.log("value: ")
-                            console.log(value)
-                            return (
-                                <tr key={value.id}>
-                                    <td>{value.id}</td>
-                                    <td>{value.name}</td>
-                                    <td>{value.note}</td>
-                                    <td>
-                                        <div className="input-group">
-                                            <input type="text" className="form-control" placeholder="Client name" />
-                                            <div className="input-group-addon">
+            <div>
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Client name" />
+                    <div className="input-group-addon">
 
-                                                {/* To use font-awsome:
-                                                    - s1: npm install --save font-awesome
-                                                    - s2: import 'font-awesome/css/font-awesome.min.css';
-                                                */}
-                                                <i className="fa fa-search fa-lg" onClick={this.btnOKClick}></i>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {/* 
+                        {/* To use font-awsome:
+                            - s1: npm install --save font-awesome
+                            - s2: import 'font-awesome/css/font-awesome.min.css';
+                        */}
+                        <i className="fa fa-search fa-lg" onClick={this.btnOKClick}></i>
+                    </div>
+                </div>
+                <table className="table table-dark table-hover" >
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Notes</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.item.map(value => {
+                                console.log("value: ")
+                                console.log(value)
+                                return (
+                                    <tr key={value.id}>
+                                        <td>{value.id}</td>
+                                        <td>{value.name}</td>
+                                        <td>{value.note}</td>
+                                        <td>
+                                            {/* 
                                         To use Bootstrap 4: 
                                         - s1: npm install --save reactstrap@next react react-dom
                                         - s2: import needed components. ex: import { Alert } from 'reactstrap';
 
                                         * For more information, visit page: https://reactstrap.github.io/
                                         */}
-                                        <Button className="btn" color="danger">Danger!</Button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table >
+                                            <Button className="btn" color="danger">Danger!</Button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table >
+            </div>
         )
     }
 }
