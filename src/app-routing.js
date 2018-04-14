3b0d8cb9dbea2579b9e2d6b78e7c80e0edb8d335
Bo <mctuan@MACs-MacBook-Pro-6.local>
2018-04-14 15:44:01 +0700

    Create new Register andForget Password components"

--- src/app-routing.js
+++ src/app-routing.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+// Components
 import Login from './components/Login/Login';
 import Main from './components/main';
+import Register from './components/Login/Register';
+import ForgetPassword from './components/Login/ForgetPassword';
 
 const Router = () => (
     <Switch>
-        <Route exact path='/' component={Login} />
+
+        {/* The page will redirect to login page if url */}
+        <Route exact path='/' render={() => <Redirect to="/login" />} />
+
+        {/* Login Components */}
+        <Route path='/login' component={Login} />
         <Route path='/home' component={Main} />
+        <Route path='/register' component={Register} />
+        <Route path='/forget_password' component={ForgetPassword} />
     </Switch>
 )
 
