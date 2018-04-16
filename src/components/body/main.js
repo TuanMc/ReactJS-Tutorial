1e8991123ff64a16137686980737d39a33e53d5e
TuanMc <maichungtuan@gmail.com>
2018-04-16 15:06:48 +0700

    Add login service

---
+++ src/components/body/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// Components
+import Navigation from '../common/Navigation';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+        <Navigation />
+      </div>
+    )
+  }
+}
